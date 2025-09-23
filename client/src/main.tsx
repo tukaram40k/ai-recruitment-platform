bfbf5417d1b9969cf60609debfad777fc1cf5df2
tukaram40k <tukaram40k@gmail.com>
2025-09-23 14:07:08 +0300

    add URL-based routing with React Router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route} from 'react-router'
 import './index.css'
 import App from './components/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+    </Routes>
+  </BrowserRouter>,
 )
