d333c41c5b61262ad6fe5b08c25f52ced3d86c14
tukaram40k <tukaram40k@gmail.com>
2025-09-25 21:18:26 +0300

    add login ui

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,16 +2,18 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route} from 'react-router'
 import './index.css'
 import App from './components/App.tsx'
-import LoginPage from "./components/LoginPage.tsx";
+import LoginSelector from "./components/auth/LoginSelector.tsx";
 import Placeholder from "./components/Placeholder.tsx";
+import LoginPage from "./components/auth/LoginPage.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/login/candidate' element={<Placeholder />} />
-      <Route path='/login/recruiter' element={<Placeholder />} />
+      <Route path='/login' element={<LoginSelector />} />
+      <Route path='/login/candidate' element={<LoginPage />} />
+      <Route path='/login/recruiter' element={<LoginPage />} />
+      <Route path='/placeholder' element={<Placeholder />} />
     </Routes>
   </BrowserRouter>,
 )
